42a9430186cf0cbae82145b95d838ec2412088d7
Elmira Matyeva <elmira.matyeva@gmail.com>
2022-03-29 07:20:31 +0600

    Added colors to status badges

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,20 @@ class Task < ApplicationRecord
   validates :status, inclusion: { in: ['not-started', 
                                       'in-progress', 
                                       'complete'] }
-  STATUS_OPTIONS = [['Not started', 'not-started'],
-                    ['In progress','in-progress'],
-                    ['Complete', 'complete']]
+  STATUS_OPTIONS = [
+    ['Not started', 'not-started'],
+    ['In progress','in-progress'],
+    ['Complete', 'complete']
+  ]
+
+  def badge_color
+    case status 
+    when 'not-started'
+      'secondary'
+    when 'in-progress'
+      'info'
+    when 'complete'
+      'success'
+    end
+  end
 end
